e1e443a22873f19fd0d1a846e23e0a4867ce08fa
gpmatheus <63263215+gpmatheus@users.noreply.github.com>
2025-06-16 18:46:35 -0300

    image - next image

--- plot_training.py
+++ plot_training.py
@@ -1,7 +1,7 @@
 
 import json
 
-with open('first-run/n0-model_info.json', 'r') as json_data:
+with open('third-run(image-next_image)/n1-model_info.json', 'r') as json_data:
     d = json.load(json_data)
     json_data.close()
     validmse = d['validmse']
--- test.py
+++ test.py
@@ -6,27 +6,30 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import json
 from sklearn.metrics import mean_squared_error
+import datetime
 
-model = keras.models.load_model('first-run/452-126.68.keras', compile=False)
+model = keras.models.load_model('third-run(image-next_image)/268-120.81.keras', compile=False)
 model.compile(optimizer='adam', loss=keras.losses.MeanSquaredError(), metrics=['mse'])
 
 img_w = 64
 rotations = 10
 angles = tf.cast(tf.linspace(0, 360, rotations), tf.float32)
 channels = [0, 3]
+generated_channels = [0]
+ds = [f'{i}' for i in ['TCIR-ATLN_EPAC_WPAC.h5', 'TCIR-CPAC_IO_SH.h5']]
 
-path = 'first-run/_test.h5'
-with h5py.File(path) as file:
-    images = file['matrix'][:]
-    info = file['info'][:]
-    data_len = images.shape[0]
+# path = 'first-run/_test.h5'
+# with h5py.File(path) as file:
+#     images = file['matrix'][:]
+#     info = file['info'][:]
+#     data_len = images.shape[0]
 
-# def get_images_slice(images_shape, width):
-#     start = images_shape[1] // 2 - width // 2
-#     end = images_shape[1] // 2 + width // 2
-#     return slice(start, end)
+def get_images_slice(images_shape, width):
+    start = images_shape[1] // 2 - width // 2
+    end = images_shape[1] // 2 + width // 2
+    return slice(start, end)
 
-# path = 'TCIR-ALL_2017.h5'
+path = 'TCIR-ALL_2017.h5'
 
 # with h5py.File(path) as file:
 #     images = file['matrix']
@@ -35,12 +38,12 @@ with h5py.File(path) as file:
 #     info = pd.read_hdf(path, key='info', mode='r')['Vmax']
 #     data_len = images.shape[0]
 
-# with open('second-run/n0-model_info.json', 'r') as json_data:
-#     data = json.load(json_data)
-#     json_data.close()
-#     params = data['normparams']
-#     means = [params[ch]['mean'] for ch in range(len(channels))]
-#     stds = [params[ch]['std'] for ch in range(len(channels))]
+with open('third-run(image-next_image)/n1-model_info.json', 'r') as json_data:
+    data = json.load(json_data)
+    json_data.close()
+    params = data['normparams']
+    means = [params[ch]['mean'] for ch in range(len(channels))]
+    stds = [params[ch]['std'] for ch in range(len(channels))]
 
 # print('normalizing images...')
 # def clean_images(images):
@@ -50,6 +53,85 @@ with h5py.File(path) as file:
 # images = clean_images(images)
 # for i in range(len(channels)):
 #     images[:, :, :, i] = (images[:, :, :, i] - means[i]) / stds[i]
+
+def clean_images(images):
+    images = np.nan_to_num(images, copy=False)
+    images[images > 1000] = 0
+    return images
+
+def pre_process(width, means, stds, batch=1024):
+    indexes = []
+    info = pd.read_hdf(path, key='info', mode='r')[['ID', 'Vmax', 'time']]
+    ids = list(info['ID'].unique())
+    for ide in ids:
+        # Filtra o DataFrame para o ID atual
+        sub_info = info[info['ID'] == ide]
+        
+        # Converte a coluna 'time' para datetime
+        sub_info = sub_info.copy()
+        sub_info['time_dt'] = sub_info['time'].apply(lambda t: datetime.datetime.strptime(str(t), "%Y%m%d%H"))
+        
+        # Ordena pelo datetime e pega os índices ordenados
+        sorted_idx = sub_info.sort_values('time_dt').index.tolist()
+        indexes.append(sorted_idx)
+    
+    # corta imagem grande o suficiente para poder rotacionar
+    rotation_width = int(np.ceil(np.sqrt((width ** 2) * 2)))
+    if rotation_width % 2 != 0:
+        rotation_width += 1
+    
+    with h5py.File('test.h5', 'w') as f, h5py.File(path, mode='r') as src:
+        images = src['matrix']
+        info = pd.read_hdf(path, key='info', mode='r')[['ID', 'Vmax', 'time']]
+        slc = get_images_slice(images.shape, rotation_width)
+        for id_idx, cyclone_id in enumerate(ids): # itera os ciclones (índice e ID)
+            cy_idxs = list(indexes[id_idx]) # lista de índices das imagens de um ciclone
+            cyclone_images = images[cy_idxs, slc, slc]
+            cyclone_images = cyclone_images[:, :, :, channels]
+            cyclone_info = info.iloc[cy_idxs]
+            
+            cyclone_images = clean_images(cyclone_images)
+            for j, (m, s) in enumerate(zip(means, stds)):
+                cyclone_images[:, :, :, j] -= m
+                cyclone_images[:, :, :, j] /= s
+
+            generated_channels_idx = [channels.index(ch) for ch in generated_channels]
+
+            new_channels = []
+            for j in range(cyclone_images.shape[0] - 1):
+                current_img = np.expand_dims(cyclone_images[j, :, :, generated_channels_idx][0], axis=-1)
+                next_img = np.expand_dims(cyclone_images[j + 1, :, :, generated_channels_idx][0], axis=-1)
+                new_ch = current_img - next_img
+                new_channels.append(new_ch)
+            new_channels = np.array(new_channels)
+
+            cyclone_images = cyclone_images[:-1]
+            cyclone_info = cyclone_info[:-1]
+            cyclone_info = cyclone_info['Vmax']
+            cyclone_images = np.concatenate((cyclone_images, new_channels), axis=-1)
+            img_new_shape = cyclone_images.shape[1:]
+
+            if cyclone_images.shape[0] > 0:
+                if 'matrix' not in f:
+                    f.create_dataset('matrix', shape=(0,) + img_new_shape, maxshape=(None,) + img_new_shape)
+                f['matrix'].resize(f['matrix'].shape[0] + cyclone_images.shape[0], axis=0)
+                f['matrix'][-cyclone_images.shape[0]:] = cyclone_images
+                if 'info' not in f:
+                    f.create_dataset('info', shape=(0,), maxshape=(None,))
+                f['info'].resize(f['info'].shape[0] + cyclone_info.shape[0], axis=0)
+                f['info'][-cyclone_info.shape[0]:] = cyclone_info
+
+print('normalizing images...')
+# images = clean_images(images)
+# for i in range(len(channels)):
+#     images[:, :, :, i] = (images[:, :, :, i] - means[i]) / stds[i]
+
+pre_process(img_w, means, stds)
+
+with h5py.File('test.h5', mode='r') as file:
+    images = file['matrix'][:]
+    info = file['info'][:]
+    data_len = images.shape[0]
 
 def parse_example(image):
     image = tf.cast(image, tf.float32)
